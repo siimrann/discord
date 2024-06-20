c5f8c418a49513b036c7c1e24e4d1cb34d1b86a8
SImran Tyagi <111629975+siimrann@users.noreply.github.com>
2024-06-20 14:59:06 +0530

    complete

---
+++ DETAILS.md
@@ -0,0 +1,9 @@
+TECHNOLOGY USED :)
+
+PlanetScale for database
+socket-io for live interaction
+
+
+WEBSITE :)
+
+discord-sql-production.up.railway.app
---
+++ components/providers/query-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import { useState } from "react";
+
+
+export const QueryProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
